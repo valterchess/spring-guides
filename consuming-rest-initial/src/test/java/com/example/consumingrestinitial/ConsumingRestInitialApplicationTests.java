8cf65555ebf48f5132ebedd4f6a7878caade392e
Valter Silva <valterrekabano7@gmail.com>
2021-08-19 00:59:32 -0300

    adicionando app que consome de uma api

---
+++ consuming-rest-initial/src/test/java/com/example/consumingrestinitial/ConsumingRestInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestinitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
